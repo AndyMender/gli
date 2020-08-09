5e246529f0dc1413be2f549c0a432e5ccd306d04
Andy Mender <andymenderunix@gmail.com>
2020-08-09 19:33:23 +0200

    Minor clean-up to gli/gli.hpp

--- gli/gli.hpp
+++ gli/gli.hpp
@@ -24,17 +24,15 @@
 
 #pragma once
 
-#define GLI_VERSION					83
+// TODO: should this not use cmake variables?
+#define GLI_VERSION                             83
 #define GLI_VERSION_MAJOR			0
-#define GLI_VERSION_MINOR			8
-#define GLI_VERSION_PATCH			3
-#define GLI_VERSION_REVISION		0
+#define GLI_VERSION_MINOR                       8
+#define GLI_VERSION_PATCH                       3
+#define GLI_VERSION_REVISION                    0
 
 /// Namespace where all the classes and functions provided by GLI are exposed
-namespace gli
-{
-
-}//namespace gli
+namespace gli {}  //namespace gli
 
 #include "format.hpp"
 #include "target.hpp"
